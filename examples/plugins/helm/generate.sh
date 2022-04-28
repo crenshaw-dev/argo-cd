9ea3155f386747415938cf884f50194295dd13f2
Michael Crenshaw <michael@crenshaw.dev>
2022-04-28 10:53:34 -0400

    use printf instead of echo
    Signed-off-by: Michael Crenshaw <michael@crenshaw.dev>

--- examples/plugins/helm/generate.sh
+++ examples/plugins/helm/generate.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+
 ARGUMENTS=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "values-files").array | .[] | "--values=" + .')
 PARAMETERS=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "helm-parameters").map | to_entries | map("\(.key)=\(.value)") | .[] | "--set=" + .')
-ARGUMENTS="$ARGUMENTS\n$PARAMETERS"
+ARGUMENTS=$(printf "%s\n%s" "$ARGUMENTS" "$PARAMETERS")
+
 echo "$ARGUMENTS" | xargs helm template .
