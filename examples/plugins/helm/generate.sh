a02632e8d313c38748d113b2d0e0ddde6de63f05
Michael Crenshaw <michael@crenshaw.dev>
2022-04-28 13:31:03 -0400

    handle empty params
    Signed-off-by: Michael Crenshaw <michael@crenshaw.dev>

--- examples/plugins/helm/generate.sh
+++ examples/plugins/helm/generate.sh
@@ -2,6 +2,5 @@
 
 ARGUMENTS=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "values-files").array | .[] | "--values=" + .')
 PARAMETERS=$(echo "$ARGOCD_APP_PARAMETERS" | jq -r '.[] | select(.name == "helm-parameters").map | to_entries | map("\(.key)=\(.value)") | .[] | "--set=" + .')
-ARGUMENTS=$(printf "%s\n%s" "$ARGUMENTS" "$PARAMETERS")
 
-echo "$ARGUMENTS" | xargs helm template .
+echo ". $ARGUMENTS $PARAMETERS" | xargs helm template
